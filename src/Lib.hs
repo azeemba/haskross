71f99d0c87334e823d8948007666fd1fd281c8de
Azeem Bande-Ali <a.bandeali@gmail.com>
2019-04-02 21:30:55 -0400

    Node contains clues + find fix point

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,21 +2,41 @@ module Lib
     ( someFunc,
       Node(Node),
       Words,
-      filter_node,
+      fix_compress,
+      compress_node,
       filter_chars,
       does_char_fit,
       does_word_fit
     ) where
 
+import Control.Monad.Fix
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-data Node = Node [Char] Int Int deriving (Show, Eq)
+data Node = Node Int Words Int Words [Char] | NodeEmpty deriving (Show, Eq)
+
 type Words = [String]
 
-filter_node :: Node -> Words -> Node
-filter_node (Node chars row col) words = Node filtered_chars row col
-  where filtered_chars = filter_chars (filter_chars chars row words) col words
+fix_compress :: Node -> Node
+fix_compress = \node -> snd
+  ( until (\(before, after) -> before == after)
+          (\(before, after) -> (after, compress_node after))
+          (NodeEmpty, node)
+  )
+
+compress_node :: Node -> Node
+compress_node (Node ind1 words1 ind2 words2 chars) =
+  let words1o = filter_words ind1 words1 chars
+      words2o = filter_words ind2 words2 chars
+      charso  = filter_chars (filter_chars chars ind1 words1o) ind2 words2o
+  in  Node ind1 words1o ind2 words2o charso
+
+filter_words :: Int -> Words -> [Char] -> Words
+filter_words ind words chars =
+  filter (\w -> does_word_fit_cs w ind chars) words
+
+does_word_fit_cs :: String -> Int -> [Char] -> Bool
+does_word_fit_cs w i cs = any (\c -> does_word_fit c i w) cs
 
 filter_chars :: [Char] -> Int -> Words -> [Char]
 filter_chars chars index words =
--- test/Spec.hs
+++ test/Spec.hs
@@ -22,13 +22,22 @@ main = hspec $ do
     it "returns false when no words match" $ do
       does_char_fit 'a' 1 ["bbb", "notmatching", "_"] `shouldBe` False
 
-  describe "filter_node" $ do
+  describe "compress_node" $ do
     it "results in 0 chars when word list emptyh" $ do
-      filter_node (Node ['a'] 0 1) [] `shouldBe` Node [] 0 1
+      compress_node (Node 0 [] 1 [] ['c']) `shouldBe` Node 0 [] 1 [] []
     it "should have chars empty if they start empty" $ do
-      filter_node (Node [] 0 1) ["abc"] `shouldBe` Node [] 0 1
+      compress_node (Node 1 ["abc"] 0 ["bac"] []) `shouldBe` Node 1 [] 0 [] []
+  
+  describe "fix_compress" $ do
+    it "should leave stable state unchanged" $ do
+      fix_compress (Node 0 ["ab", "bc"] 1 ["_a", "_b"] ['a', 'b'])
+        `shouldBe` Node 0 ["ab", "bc"] 1 ["_a", "_b"] ['a', 'b']
+    it "should remove first clue words with no match" $ do
+      fix_compress (Node 0 ["ac", "bd"] 1 ["eb", "fb"] ['a', 'b'])
+        `shouldBe` Node 0 ["bd"] 1 ["eb", "fb"] ['b']
+    it "should remove second clue words with no match" $ do
+      fix_compress (Node 1 ["eb", "fb"] 0 ["ac", "bd"] ['a', 'b'])
+        `shouldBe` Node 1 ["eb", "fb"] 0 ["bd"] ['b']
     it "should remove characters with no match" $ do
-      filter_node (Node ['a', 'b', 'd'] 0 1) ["abc", "bac"]
-        `shouldBe` Node ['a', 'b'] 0 1
-    it "should remove characters with only row or only column match" $ do
-      filter_node (Node ['a', 'b', 'd'] 0 1) ["abc"] `shouldBe` Node [] 0 1
+      compress_node (Node 0 ["a", "b"] 0 ["a", "b"] ['a', 'b', 'd'])
+        `shouldBe` Node 0 ["a", "b"] 0 ["a", "b"] ['a', 'b']
