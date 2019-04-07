2bc377069fefe4cbf71012139d960fb06371f2e9
Azeem Bande-Ali <a.bandeali@gmail.com>
2019-04-07 13:31:49 -0400

    Optimize so stress test takes subsecond time
    The stress test of 4x4 grid with 10k words was taking 75-80 seconds and
    now it takes less than half a second.
    
    Changes:
     - Store all words as a vector
     - Each node stores indices into this vector as a IntSet (which is a set
     optimized for integer values)
    
    The implementation and test had to be chanaged wholesale due to
    interface changes everywhere.

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,10 +16,12 @@ module Lib
 
 import Data.List
 import qualified Data.Vector as Vector
+import qualified Data.IntSet as IntSet
 
-data Node = Node Int Words Int Words [Char] | NodeEmpty deriving (Show, Eq)
+data Node = Node Int Words Int Words [Char] AllWords | NodeEmpty deriving (Show, Eq)
 
-type Words = [String]
+type Words = IntSet.IntSet
+type AllWords = Vector.Vector String
 
 data WhichClue = First | Second deriving (Enum, Eq, Show)
 
@@ -29,7 +31,7 @@ data WhichClue = First | Second deriving (Enum, Eq, Show)
 type Clue = (WhichClue, [Int])
 data Grid = Grid (Vector.Vector Node) [Clue] deriving (Eq)
 instance Show Grid where
-  show (Grid nodes clues) = 
+  show (Grid nodes clues) =
       Vector.foldr (\node output -> if (length output > 100) then output else output ++ show node) "" nodes
 
 
@@ -48,7 +50,7 @@ iterate_on_grid (Grid nodes clues) =
 
 find_and_merge_clue :: Clue -> Vector.Vector Node -> Vector.Vector Node
 find_and_merge_clue (which, indices) nodes =
-  let specific_nodes  = map (\ind -> Vector.unsafeIndex nodes ind) indices
+  let specific_nodes  = map (\ind -> nodes Vector.! ind) indices
       specific_nodes' = merge_clues which specific_nodes
   in  Vector.unsafeUpdate nodes (Vector.fromList (zip indices specific_nodes'))
 
@@ -60,7 +62,7 @@ find_and_merge_clue (which, indices) nodes =
 ind2pos :: Int -> Int -> (Int, Int)
 ind2pos i n = quotRem i n
 
-make_square_grid :: Int -> Words -> Grid
+make_square_grid :: Int -> AllWords -> Grid
 make_square_grid size words =
   let nodes =
         map (\i -> make_square_node i size words) (take (size * size) [0 ..])
@@ -69,28 +71,32 @@ make_square_grid size words =
           ++ map (\i -> (Second, take size [i, i + size ..])) [0 .. size - 1]
   in  Grid (Vector.fromList nodes) clues
 
-make_square_node :: Int -> Int -> Words -> Node
+make_square_node :: Int -> Int -> AllWords -> Node
 make_square_node i size words =
-  let (x, y) = ind2pos i size in Node x words y words ['a' .. 'z']
+  let (x, y)  = ind2pos i size
+      wordset = IntSet.fromDistinctAscList (take (Vector.length words) [0 ..])
+  in  Node x wordset y wordset ['a' .. 'z'] words
 
 -- The two nodes are part of the same clue. So we can filter out words
 -- that are not in both
 merge_clues :: WhichClue -> [Node] -> [Node]
 merge_clues which_clue nodes =
   let words_lists = map (\node -> extract_clue (which_clue, node)) nodes
-      merged = if null words_lists then [] else foldr1 intersect words_lists
+      merged      = if null words_lists
+        then IntSet.empty
+        else foldr1 (IntSet.intersection) words_lists
   in  map (\node -> inject_clue (which_clue, node) merged) nodes
 
 
 extract_clue :: (WhichClue, Node) -> Words
-extract_clue (First , (Node _ words1 _ _ _)) = words1
-extract_clue (Second, (Node _ _ _ words2 _)) = words2
+extract_clue (First , (Node _ words1 _ _ _ _)) = words1
+extract_clue (Second, (Node _ _ _ words2 _ _)) = words2
 
 inject_clue :: (WhichClue, Node) -> Words -> Node
-inject_clue (First, (Node ind1 _ ind2 words2 chars)) words1 =
-  Node ind1 words1 ind2 words2 chars
-inject_clue (Second, (Node ind1 words1 ind2 _ chars)) words2 =
-  Node ind1 words1 ind2 words2 chars
+inject_clue (First, (Node ind1 _ ind2 words2 chars all)) words1 =
+  Node ind1 words1 ind2 words2 chars all
+inject_clue (Second, (Node ind1 words1 ind2 _ chars all)) words2 =
+  Node ind1 words1 ind2 words2 chars all
 
 
 find_stable :: Eq a => (a -> a) -> a -> a -> a
@@ -104,26 +110,38 @@ fix_compress :: Node -> Node
 fix_compress = find_stable compress_node NodeEmpty
 
 compress_node :: Node -> Node
-compress_node (Node ind1 words1 ind2 words2 chars) =
-  let words1o = filter_words ind1 words1 chars
-      words2o = filter_words ind2 words2 chars
-      charso  = filter_chars (filter_chars chars ind1 words1o) ind2 words2o
-  in  Node ind1 words1o ind2 words2o charso
+compress_node (Node ind1 words1 ind2 words2 chars allWords) =
+  let
+    words1o = filter_words ind1 words1 chars allWords
+    words2o = filter_words ind2 words2 chars allWords
+    charso  = filter_chars (filter_chars chars ind1 words1o allWords)
+                           ind2
+                           words2o
+                           allWords
+  in
+    Node ind1 words1o ind2 words2o charso allWords
 
-filter_words :: Int -> Words -> [Char] -> Words
-filter_words ind words chars =
-  filter (\w -> does_word_fit_cs w ind chars) words
+filter_words :: Int -> Words -> [Char] -> AllWords -> Words
+filter_words ind words chars allWords =
+  IntSet.filter (\w -> does_word_fit_cs w ind chars allWords) words
 
-does_word_fit_cs :: String -> Int -> [Char] -> Bool
-does_word_fit_cs w i cs = any (\c -> does_word_fit c i w) cs
+does_word_fit_cs :: Int -> Int -> [Char] -> AllWords -> Bool
+does_word_fit_cs w i cs allWords = any (\c -> does_word_fit c i allWords w) cs
 
-filter_chars :: [Char] -> Int -> Words -> [Char]
-filter_chars chars index words =
-  filter (\c -> does_char_fit c index words) chars
+filter_chars :: [Char] -> Int -> Words -> AllWords -> [Char]
+filter_chars chars index words allWords =
+  filter (\c -> does_char_fit c index words allWords) chars
 
-does_char_fit :: Char -> Int -> Words -> Bool
-does_char_fit c index words = any (does_word_fit c index) words
+does_char_fit :: Char -> Int -> Words -> AllWords -> Bool
+does_char_fit c index words allWords = any
+  id
+  ( IntSet.foldr
+    (\wordI answers -> (does_word_fit c index allWords wordI) : answers)
+    []
+    words
+  )
 
-does_word_fit :: Char -> Int -> String -> Bool
-does_word_fit c index word =
-  if index < length word then (word !! index) == c else False
+does_word_fit :: Char -> Int -> AllWords -> Int -> Bool
+does_word_fit c index allWords wordI =
+  let word = allWords Vector.! wordI
+  in  if index < length word then (word !! index) == c else False
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,110 +1,178 @@
 import Test.Hspec
 import qualified Data.Vector as Vector
+import qualified Data.IntSet as IntSet
+import Data.Maybe
 import Control.Exception (evaluate)
 import Lib
 import Data.List
+
+make_node :: Int -> [String] -> Int -> [String] -> [Char] -> Node
+make_node ind1 words1 ind2 words2 chars =
+  let all_words = Vector.fromList (words1 ++ words2)
+      wordsI1   = IntSet.fromDistinctAscList (take (length words1) [0 ..])
+      wordsI2 =
+        IntSet.fromDistinctAscList (take (length words2) [(length words1) ..])
+  in  Node ind1 wordsI1 ind2 wordsI2 chars all_words
+
+make_node_with_words
+  :: Int
+  -> [String]
+  -> Int
+  -> [String]
+  -> [Char]
+  -> Vector.Vector String
+  -> Node
+make_node_with_words ind1 words1 ind2 words2 chars all_words =
+  let wordsI1 = IntSet.fromList
+        $ map (\word -> fromJust (Vector.elemIndex word all_words)) words1
+      wordsI2 = IntSet.fromList
+        $ map (\word -> fromJust (Vector.elemIndex word all_words)) words2
+  in  Node ind1 wordsI1 ind2 wordsI2 chars all_words
+
 
 main :: IO ()
 main = hspec $ do
   describe "does_word_fit" $ do
     it "handles simple positive case" $ do
-      does_word_fit 'a' 1 "_a_" `shouldBe` True
+      let words = Vector.fromList ["_a_"]
+      does_word_fit 'a' 1 words 0 `shouldBe` True
     it "handles index larger than word size" $ do
-      does_word_fit 'a' 3 "_a_" `shouldBe` False
+      let words = Vector.fromList ["_a_"]
+      does_word_fit 'a' 3 words 0 `shouldBe` False
     it "handles simple negative case" $ do
-      does_word_fit 'a' 1 "___" `shouldBe` False
+      let words = Vector.fromList ["___"]
+      does_word_fit 'a' 1 words 0 `shouldBe` False
 
   describe "does_char_fit" $ do
     it "handles empty words list" $ do
-      does_char_fit 'a' 1 [] `shouldBe` False
+      let words = Vector.fromList []
+      does_char_fit 'a' 1 (IntSet.fromDistinctAscList []) words `shouldBe` False
     it "returns true when char matches a words" $ do
-      does_char_fit 'a' 1 ["_a_"] `shouldBe` True
+      let words  = Vector.fromList ["_a_"]
+      let wordsI = IntSet.fromDistinctAscList [0]
+      does_char_fit 'a' 1 wordsI words `shouldBe` True
     it "returns true even with multiple words" $ do
-      does_char_fit 'a' 1 ["_a_", "____", "_"] `shouldBe` True
+      let words  = Vector.fromList ["_a_", "____", "_"]
+      let wordsI = IntSet.fromDistinctAscList [0, 1, 2]
+      does_char_fit 'a' 1 wordsI words `shouldBe` True
     it "returns false when no words match" $ do
-      does_char_fit 'a' 1 ["bbb", "notmatching", "_"] `shouldBe` False
+      let words  = Vector.fromList ["bbb", "notmatching", "_"]
+      let wordsI = IntSet.fromDistinctAscList [0, 1, 2]
+      does_char_fit 'a' 1 wordsI words `shouldBe` False
 
   describe "compress_node" $ do
     it "results in 0 chars when word list emptyh" $ do
-      compress_node (Node 0 [] 1 [] ['c']) `shouldBe` Node 0 [] 1 [] []
+      compress_node (make_node 0 [] 1 [] ['c'])
+        `shouldBe` make_node 0 [] 1 [] []
     it "should have chars empty if they start empty" $ do
-      compress_node (Node 1 ["abc"] 0 ["bac"] []) `shouldBe` Node 1 [] 0 [] []
+      compress_node (make_node 1 ["abc"] 0 ["bac"] []) `shouldBe` Node
+        1
+        IntSet.empty
+        0
+        IntSet.empty
+        []
+        (Vector.fromList ["abc", "bac"])
 
   describe "fix_compress" $ do
     it "should leave stable state unchanged" $ do
-      fix_compress (Node 0 ["ab", "bc"] 1 ["_a", "_b"] ['a', 'b'])
-        `shouldBe` Node 0 ["ab", "bc"] 1 ["_a", "_b"] ['a', 'b']
+      fix_compress (make_node 0 ["ab", "bc"] 1 ["_a", "_b"] ['a', 'b'])
+        `shouldBe` make_node 0 ["ab", "bc"] 1 ["_a", "_b"] ['a', 'b']
     it "should remove first clue words with no match" $ do
-      fix_compress (Node 0 ["ac", "bd"] 1 ["eb", "fb"] ['a', 'b'])
-        `shouldBe` Node 0 ["bd"] 1 ["eb", "fb"] ['b']
+      let all_words = Vector.fromList ["ac", "bd", "eb", "fb"]
+      let wordsI1   = IntSet.fromDistinctAscList [0, 1]
+      let wordsI2   = IntSet.fromDistinctAscList [2, 3]
+      fix_compress (Node 0 wordsI1 1 wordsI2 ['a', 'b'] all_words)
+        `shouldBe` Node 0
+                        (IntSet.fromAscList [1])
+                        1
+                        (IntSet.fromAscList [2, 3])
+                        ['b']
+                        all_words
     it "should remove second clue words with no match" $ do
-      fix_compress (Node 1 ["eb", "fb"] 0 ["ac", "bd"] ['a', 'b'])
-        `shouldBe` Node 1 ["eb", "fb"] 0 ["bd"] ['b']
+      fix_compress (make_node 1 ["eb", "fb"] 0 ["ac", "bd"] ['a', 'b'])
+        `shouldBe` make_node_with_words
+                     1
+                     ["eb", "fb"]
+                     0
+                     ["bd"]
+                     ['b']
+                     (Vector.fromList ["eb", "fb", "ac", "bd"])
     it "should remove characters with no match" $ do
-      compress_node (Node 0 ["a", "b"] 0 ["a", "b"] ['a', 'b', 'd'])
-        `shouldBe` Node 0 ["a", "b"] 0 ["a", "b"] ['a', 'b']
+      compress_node (make_node 0 ["a", "b"] 0 ["a", "b"] ['a', 'b', 'd'])
+        `shouldBe` make_node 0 ["a", "b"] 0 ["a", "b"] ['a', 'b']
 
   describe "merge nodes in clues" $ do
     it "should remove words that aren't in both nodes" $ do
-      let node1   = Node 0 ["both", "single"] 0 [] ['a', 'b']
-      let node2   = Node 0 ["one", "both"] 0 [] ['a', 'b']
-      let result1 = Node 0 ["both"] 0 [] ['a', 'b']
-      let result2 = Node 0 ["both"] 0 [] ['a', 'b']
+      let all_words = Vector.fromList ["both", "single", "one", "both"]
+      let node1 =
+            make_node_with_words 0 ["both", "single"] 0 [] ['a', 'b'] all_words
+      let node2 =
+            make_node_with_words 0 ["one", "both"] 0 [] ['a', 'b'] all_words
+      let result1 = make_node_with_words 0 ["both"] 0 [] ['a', 'b'] all_words
+      let result2 = make_node_with_words 0 ["both"] 0 [] ['a', 'b'] all_words
       merge_clues First [node1, node2] `shouldBe` [result1, result2]
     it "should leave valid clues unchanged" $ do
       let words = ["a", "b", "c"]
       let chars = ['a', 'b']
-      let node  = Node 0 words 0 words chars
+      let node  = make_node 0 words 0 words chars
       merge_clues First [node, node] `shouldBe` [node, node]
     it "should empty out words when any words is empty" $ do
-      let words1 = ["ab", "cd", "ef"]
-      let words2 = []
-      let chars  = ['a' .. 'z']
-      let node1  = Node 0 words1 0 words2 chars
-      let node2  = Node 1 words2 0 words2 chars
+      let words1    = ["ab", "cd", "ef"]
+      let words2    = []
+      let chars     = ['a' .. 'z']
+      let all_words = Vector.fromList words1
+      let node1     = make_node_with_words 0 words1 0 words2 chars all_words
+      let node2     = make_node_with_words 1 words2 0 words2 chars all_words
       merge_clues First [node1, node2]
-        `shouldBe` [Node 0 [] 0 [] chars, Node 1 [] 0 [] chars]
+        `shouldBe` [ make_node_with_words 0 [] 0 [] chars all_words
+                   , make_node_with_words 1 [] 0 [] chars all_words
+                   ]
     it "should empty out when no match possible" $ do
-      let words1 = ["ab", "cd", "ef"]
-      let words2 = ["pq", "rs", "tv"]
-      let chars  = ['a' .. 'z']
-      let node1  = Node 0 words1 0 [] chars
-      let node2  = Node 1 words2 0 [] chars
+      let words1    = ["ab", "cd", "ef"]
+      let words2    = ["pq", "rs", "tv"]
+      let chars     = ['a' .. 'z']
+      let all_words = Vector.fromList (words1 ++ words2)
+      let node1     = make_node_with_words 0 words1 0 [] chars all_words
+      let node2     = make_node_with_words 1 words2 0 [] chars all_words
       merge_clues First [node1, node2]
-        `shouldBe` [Node 0 [] 0 [] chars, Node 1 [] 0 [] chars]
+        `shouldBe` [ make_node_with_words 0 [] 0 [] chars all_words
+                   , make_node_with_words 1 [] 0 [] chars all_words
+                   ]
 
   describe "square grid" $ do
     it "should look right" $ do
-      let words = ["bat", "cat"]
-      let chars = ['a' .. 'z']
+      let words  = ["bat", "cat"]
+      let wordsV = Vector.fromList words
+      let chars  = ['a' .. 'z']
       let nodes = Vector.fromList
-            [ Node 0 words 0 words chars
-            , Node 0 words 1 words chars
-            , Node 1 words 0 words chars
-            , Node 1 words 1 words chars
+            [ make_node_with_words 0 words 0 words chars wordsV
+            , make_node_with_words 0 words 1 words chars wordsV
+            , make_node_with_words 1 words 0 words chars wordsV
+            , make_node_with_words 1 words 1 words chars wordsV
             ]
       let
         clues =
           [(First, [0, 1]), (First, [2, 3]), (Second, [0, 2]), (Second, [1, 3])]
-      make_square_grid 2 words `shouldBe` Grid nodes clues
+      make_square_grid 2 wordsV `shouldBe` Grid nodes clues
 
     it "should stabilize" $ do
-      let words            = ["ab", "cd", "ef"]
+      let words            = Vector.fromList ["ab", "cd", "ef"]
       let grid             = make_square_grid 2 words
       let Grid nodes clues = grid
       let empty_nodes = Vector.fromList
-            [ Node 0 [] 0 [] []
-            , Node 0 [] 1 [] []
-            , Node 1 [] 0 [] []
-            , Node 1 [] 1 [] []
+            [ make_node_with_words 0 [] 0 [] [] words
+            , make_node_with_words 0 [] 1 [] [] words
+            , make_node_with_words 1 [] 0 [] [] words
+            , make_node_with_words 1 [] 1 [] [] words
             ]
       find_stable_grid grid `shouldBe` Grid empty_nodes clues
 
-    -- This is real slow but we still want to test this 
-    -- it "should handle larger lists" $ do
-    --   let chars = ['a'..'j']
-    --   let words = [[a, b, c, d] | a<-chars, b<-chars, c<-chars, d<-chars]
-    --   let Grid nodes clues = find_stable_grid (make_square_grid 4 words)
-    --   let Node _ res_words _ _ _ = Vector.head nodes
-    --   -- words has 10k words 
-    --   res_words `shouldBe` words
+    it "should handle larger lists" $ do
+      let chars = ['a' .. 'j']
+      let words = Vector.fromList
+            [ [a, b, c, d] | a <- chars, b <- chars, c <- chars, d <- chars ]
+      let Grid nodes clues         = find_stable_grid (make_square_grid 4 words)
+      let Node _ res_words _ _ _ _ = Vector.head nodes
+      -- words has 10k words 
+      res_words `shouldBe` IntSet.fromDistinctAscList
+        (take (Vector.length words) [0 ..])
