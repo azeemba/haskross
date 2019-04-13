3b3fbe16942a80838ae8cfa83d69c2714384fcb0
Azeem Bande-Ali <a.bandeali@gmail.com>
2019-04-13 19:54:52 -0400

    Change constrain from clue to word based and add tests
    Clue based constraints require that the full grid structure is
    predefined. The word based constraints are more flexible.

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,7 +4,9 @@ module Lib
       Nodes,
       Grid(..),
       constrain_node_letters,
-      constrain_by_clue
+      constrain_seq_by_words,
+      constrain_seq_by_word,
+      constrain_seq_by_word_at_pos
     ) where
 
 import Data.List
@@ -27,10 +29,36 @@ type ClueValue = SInt32
 constrain_node_letters :: NodeValue -> [Char] -> SBool
 constrain_node_letters node chars = bAny (\c -> literal c .== node) chars
 
-constrain_by_clue :: Clue -> Nodes -> PossibleWords -> SBool
-constrain_by_clue clue nodes all_words =
-  let node_ind_pairs = zip (map (\pos -> nodes Vector.! pos) clue) [0 ..]
-      all_nodes_fit word =
-        bAll (\(node, ind) -> literal (word !! ind) .== node) node_ind_pairs
-  in  bAny all_nodes_fit all_words
+-- constrain_by_words :: Int -> Nodes -> PossibleWords -> SBool
 
+constrain_seq_by_words :: [Int] -> Nodes -> PossibleWords -> SBool
+constrain_seq_by_words indices all_nodes all_words =
+  bAny (constrain_seq_by_word indices all_nodes) all_words
+
+-- size : 5
+-- word size : 2
+-- 0  1  2  3  4
+-- [0, 1, 2, 3]
+-- [1, 2, 3, 4]
+-- unique_pos_counts = 2
+-- = size - (word_size + 2) + 1
+-- Also add: [0, 1, 2] and [2, 3, 4]
+constrain_seq_by_word :: [Int] -> Nodes -> String -> SBool
+constrain_seq_by_word indices all_nodes word =
+  let
+    size              = length indices
+    wrapped_word      = "#" ++ word ++ "#"
+    word_len          = 2 + length word
+    unique_pos_counts = size - word_len + 1
+    word_spots        = map (\nth -> take word_len $ drop nth indices)
+                            (take unique_pos_counts [0 ..])
+    inner_constraints = bAny (\is -> constrain_seq_by_word_at_pos is all_nodes wrapped_word) word_spots
+    left_constraint = constrain_seq_by_word_at_pos (take (length word + 1) [0..]) all_nodes (word ++ "#")
+    right_constraint = constrain_seq_by_word_at_pos (reverse $ take (length word + 1) [size-1, size-2..]) all_nodes ("#" ++ word)
+  in bOr [left_constraint, inner_constraints, right_constraint]
+
+constrain_seq_by_word_at_pos :: [Int] -> Nodes -> String -> SBool
+constrain_seq_by_word_at_pos indices all_nodes word =
+  let nodes_list = map (\ind -> all_nodes Vector.! ind) indices
+      node_ind_pairs = zip nodes_list [0 ..]
+  in  bAll (\(node, ind) -> literal (word !! ind) .== node) node_ind_pairs
--- test/Spec.hs
+++ test/Spec.hs
@@ -29,28 +29,57 @@ main = hspec $ do
       let xs = getModelValues "x" result
       catMaybes xs `shouldMatchList` ['a', 'b', 'c']
 
-  describe "constrain_by_clue" $ do
+  describe "constrain_seq_by_word_at_pos" $ do
     it "should pick the sole value" $ do
-      result <- allSat $ do
-        xs <- sChar "a"
-        let nodes     = Vector.fromList [xs]
-        let clue      = [0]
-        let all_words = ["a"]
-        constrain $ constrain_by_clue clue nodes all_words
-      let xs = getModelValues "a" result
-      catMaybes xs `shouldMatchList` ['a']
-    it "should handle clue intersections" $ do
-      let node_names = ["1", "2", "3"]
+      let node_names = ["0", "1", "2"]
       result <- allSat $ do
         xs <- sChars node_names
-        let nodes     = Vector.fromList xs
-        let clue1     = [0, 1, 2]
-        let clue2     = [0] -- that's not how intersecting clues line up but whatever
-        let allWords1 = ["cat", "bag"]
-        let allWords2 = ["c", "p", "q"]
-        constrain $ constrain_by_clue clue1 nodes allWords1
-        constrain $ constrain_by_clue clue2 nodes allWords2
-      let values = map
-            (\name -> head . catMaybes $ getModelValues name result)
-            node_names
+        let nodes = Vector.fromList xs
+        let word = "cat"
+        constrain $ constrain_seq_by_word_at_pos [0, 1, 2] nodes word
+      let values = map (\name -> head . catMaybes $ getModelValues name result) node_names
       values `shouldBe` ['c', 'a', 't']
+    it "should fail with no match with inconsistent condition" $ do
+      let node_names = ["0", "1", "2"]
+      result <- sat $ do
+        xs <- sChars node_names
+        let nodes = Vector.fromList xs
+        constrain $ constrain_seq_by_word_at_pos [0, 1, 2] nodes "cat"
+        constrain $ constrain_seq_by_word_at_pos [1] nodes "z"
+      modelExists result `shouldBe` False
+    it "should handle valid intersections" $ do
+      let node_names = ["0", "1", "2"]
+      result <- allSat $ do
+        xs <- sChars node_names
+        let nodes = Vector.fromList xs
+        let word = "cat"
+        constrain $ constrain_seq_by_word_at_pos [0, 1, 2] nodes word
+        constrain $ constrain_seq_by_word_at_pos [1] nodes "a"
+      let values = map (\name -> head . catMaybes $ getModelValues name result) node_names
+      values `shouldBe` ['c', 'a', 't']
+
+  describe "constrain_seq_by_word" $ do
+    it "should set the word in all possible positions" $ do
+      let node_names = ["0", "1", "2", "3"]
+      result <- allSat $ do
+        xs <- sChars node_names
+        let nodes = Vector.fromList xs
+        let word = "at"
+        constrain $ bAll (\x -> constrain_node_letters x ('#' : ['a'..'z'])) xs
+        constrain $ constrain_seq_by_word (take 4 [0..]) nodes word
+      let result_strings = solution2strings result node_names
+      let unexpected_strings = filter (\word -> not $ "#at" `isInfixOf` word) $ filter (\word -> not $ "at#" `isInfixOf` word) result_strings
+      unexpected_strings `shouldBe` []
+      
+
+-- Takes the solution of each node_name and puts them together in the same
+-- order. So ['a','b'] and ['c', 'd'] solutions become ["ac", "bd"]
+solution2strings :: AllSatResult -> [String] -> [String]
+solution2strings result node_names = 
+  let node_values::[String]
+      node_values = map (\name -> catMaybes $ getModelValues name result) node_names
+      num_solutions = length (head node_values)
+      starting_solutions = take num_solutions (repeat "")
+      add_to_solutions values solutions = map (\(value, result) -> value : result) (zip values solutions)
+      result_string = foldr add_to_solutions starting_solutions node_values
+  in result_string
